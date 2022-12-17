e8dc7774f9364585163600418f650f52e5a3237d
Nurfikri Norjoharuddeen <nurfikri89@gmail.com>
2022-12-17 12:52:09 +0100

    Update NanoAODTest

--- workdir/RunNanoAODTest.sh
+++ workdir/RunNanoAODTest.sh
@@ -18,23 +18,13 @@ CUSTOMISE_MC=""
 CUSTOMISE_DATA=""
 
 TAG="customjmepfnano_default"
-CUSTOMISE_COMMAND_MC="from JMEPFNano.Production.custom_jme_pf_nano_10630_cff import PrepJMEPFCustomNanoAOD_MC; PrepJMEPFCustomNanoAOD_MC(process)\n"
-CUSTOMISE_COMMAND_DATA="from JMEPFNano.Production.custom_jme_pf_nano_10630_cff import PrepJMEPFCustomNanoAOD_Data; PrepJMEPFCustomNanoAOD_Data(process)\n"
+
+CUSTOMISE_COMMAND_MC="from JMEPFNano.Production.custom_jme_pf_nano_cff import PrepJMEPFCustomNanoAOD_MC; PrepJMEPFCustomNanoAOD_MC(process)\n"
+CUSTOMISE_COMMAND_DATA="from JMEPFNano.Production.custom_jme_pf_nano_cff import PrepJMEPFCustomNanoAOD_Data; PrepJMEPFCustomNanoAOD_Data(process)\n"
 
 # TAG="customjmenano_default"
 # CUSTOMISE_COMMAND_MC="from PhysicsTools.NanoAOD.custom_jme_cff import PrepJMECustomNanoAOD_MC; PrepJMECustomNanoAOD_MC(process)\n"
 # CUSTOMISE_COMMAND_DATA="from PhysicsTools.NanoAOD.custom_jme_cff import PrepJMECustomNanoAOD_Data; PrepJMECustomNanoAOD_Data(process)\n"
-
-#### NOTE TEMP:
-#### For early Run3 MC
-#### > --era Run3,run3_nanoAOD_122 --customise="PhysicsTools/NanoAOD/V10/nano_cff.nanoAOD_customizeV10"
-#### Prompt RunC 12_4 data{*} and real MC Run3 campaign
-#### > --era Run3 --customise="PhysicsTools/NanoAOD/V10/nano_cff.nanoAOD_customizeV10"
-
-# TAG="nano_default"
-# TAG="nano_mod"
-# CUSTOMISE_MC="PhysicsTools/NanoAOD/V10/nano_cff.nanoAOD_customizeV10"
-# CUSTOMISE_DATA="PhysicsTools/NanoAOD/V10/nano_cff.nanoAOD_customizeV10"
 
 mkdir -p ./dir_cfg_${TAG}/
 mkdir -p ./dir_log_${TAG}/
@@ -44,16 +34,26 @@ mkdir -p ./dir_output_${TAG}/
 # Set Global Tag depending on CMSSW version
 #
 ############################################
-if [[ $CMSSW_VERSION == *"CMSSW_12_5_"* ]]; then
-  echo "Using CMSSW_12_5_X GT"
+if [[ $CMSSW_VERSION == *"CMSSW_13_0_"* ]]; then
+  echo "Using CMSSW_13_0_X GT"
   GT_MC_2022="auto:phase1_2022_realistic"
-  # GT_MC_2022=122X_mcRun3_2021_realistic_v9
   GT_MC_2018=auto:phase1_2018_realistic
   GT_MC_2017=auto:phase1_2017_realistic
   GT_MC_2016=auto:run2_mc
   GT_MC_2016APV=auto:run2_mc
   GT_DATA_2022=auto:run3_data
-  # GT_DATA_2022=124X_dataRun3_Prompt_v4
+  GT_DATA_2018=auto:run2_data
+  GT_DATA_2017=auto:run2_data
+  GT_DATA_2016=auto:run2_data
+  GT_DATA_2016APV=auto:run2_data
+elif [[ $CMSSW_VERSION == *"CMSSW_12_6_"* ]]; then
+  echo "Using CMSSW_12_6_X GT"
+  GT_MC_2022="auto:phase1_2022_realistic"
+  GT_MC_2018=auto:phase1_2018_realistic
+  GT_MC_2017=auto:phase1_2017_realistic
+  GT_MC_2016=auto:run2_mc
+  GT_MC_2016APV=auto:run2_mc
+  GT_DATA_2022=auto:run3_data
   GT_DATA_2018=auto:run2_data
   GT_DATA_2017=auto:run2_data
   GT_DATA_2016=auto:run2_data
@@ -61,30 +61,6 @@ if [[ $CMSSW_VERSION == *"CMSSW_12_5_"* ]]; then
 elif [[ $CMSSW_VERSION == *"CMSSW_12_4_"* ]]; then
   echo "Using CMSSW_12_4_X GT"
   GT_MC_2022="auto:phase1_2022_realistic"
-  GT_MC_2018=auto:phase1_2018_realistic
-  GT_MC_2017=auto:phase1_2017_realistic
-  GT_MC_2016=auto:run2_mc
-  GT_MC_2016APV=auto:run2_mc
-  GT_DATA_2022=auto:run3_data
-  GT_DATA_2018=auto:run2_data
-  GT_DATA_2017=auto:run2_data
-  GT_DATA_2016=auto:run2_data
-  GT_DATA_2016APV=auto:run2_data
-elif [[ $CMSSW_VERSION == *"CMSSW_12_3_"* ]]; then
-  echo "Using CMSSW_12_3_X GT"
-  GT_MC_2022="auto:phase1_2021_realistic"
-  GT_MC_2018=auto:phase1_2018_realistic
-  GT_MC_2017=auto:phase1_2017_realistic
-  GT_MC_2016=auto:run2_mc
-  GT_MC_2016APV=auto:run2_mc
-  GT_DATA_2022=auto:run3_data
-  GT_DATA_2018=auto:run2_data
-  GT_DATA_2017=auto:run2_data
-  GT_DATA_2016=auto:run2_data
-  GT_DATA_2016APV=auto:run2_data
-elif [[ $CMSSW_VERSION == *"CMSSW_12_2_"* ]]; then
-  echo "Using CMSSW_12_2_X GT"
-  GT_MC_2022="auto:phase1_2021_realistic"
   GT_MC_2018=auto:phase1_2018_realistic
   GT_MC_2017=auto:phase1_2017_realistic
   GT_MC_2016=auto:run2_mc
@@ -106,14 +82,11 @@ elif [[ $CMSSW_VERSION == *"CMSSW_10_6_"* ]]; then
   GT_DATA_2017=106X_dataRun2_v35 #NanoAODv9 GT
   GT_DATA_2016=106X_dataRun2_v35 #NanoAODv9 GT
   GT_DATA_2016APV=106X_dataRun2_v35 #NanoAODv9 GT
-elif [[ $CMSSW_VERSION == *"CMSSW_10_2_"* ]]; then
-  echo "Using CMSSW_10_2_X GT"
-  GT_MC_2018=102X_upgrade2018_realistic_v21 #NanoAODv7 GT
-  GT_MC_2017=102X_mc2017_realistic_v8 #NanoAODv7 GT
-  GT_MC_2016=102X_mcRun2_asymptotic_v8 #NanoAODv7 GT
-  GT_DATA_2018=102X_dataRun2_Prompt_v16 # For 2018 Period D only
-  GT_DATA_2017=102X_dataRun2_v13
-  GT_DATA_2016=102X_dataRun2_v13
+  #
+  # Use this customise command
+  #
+  CUSTOMISE_COMMAND_MC="from JMEPFNano.Production.custom_jme_pf_nano_10630_cff import PrepJMEPFCustomNanoAOD_MC; PrepJMEPFCustomNanoAOD_MC(process)\n"
+  CUSTOMISE_COMMAND_DATA="from JMEPFNano.Production.custom_jme_pf_nano_10630_cff import PrepJMEPFCustomNanoAOD_Data; PrepJMEPFCustomNanoAOD_Data(process)\n"
 else
   echo "Cannot recognize CMSSW version: ${CMSSW_VERSION}"
   echo "GT not set. Stopping bash script now"
@@ -131,6 +104,20 @@ if [ $SAMPLE == 'MCUL18V2_QCD' ]; then
   GT=$GT_MC_2018
   MINIAOD=${INFILEPREFIX}"/store/mc/RunIISummer20UL18MiniAODv2/QCD_Pt-15to7000_TuneCP5_Flat2018_13TeV_pythia8/MINIAODSIM/106X_upgrade2018_realistic_v16_L1v1-v2/2530000/62E879B1-359D-E348-B2E8-36568A135395.root"
   NANOAOD="tree_MCUL18_miniV2_qcd.root"
+  NANOAOD_CFG=JME-RunIISummer20UL18NanoAOD_miniV2_${TAG}_cfg.py
+  ERA="Run2_2018,run2_nanoAOD_106Xv2"
+  LOGFILE="nanoaod_MCUL18_miniV2.log"
+elif [ $SAMPLE == 'MCUL18V2_TTToHadronic' ]; then
+  GT=$GT_MC_2018
+  MINIAOD=${INFILEPREFIX}"/store/mc/RunIISummer20UL18MiniAODv2/TTToHadronic_TuneCP5_13TeV-powheg-pythia8/MINIAODSIM/106X_upgrade2018_realistic_v16_L1v1-v1/100000/1F5F2A97-334A-A249-85D3-9AB5F1268838.root"
+  NANOAOD="tree_MCUL18_miniV2_tthadronic.root"
+  NANOAOD_CFG=JME-RunIISummer20UL18NanoAOD_miniV2_${TAG}_cfg.py
+  ERA="Run2_2018,run2_nanoAOD_106Xv2"
+  LOGFILE="nanoaod_MCUL18_miniV2.log"
+elif [ $SAMPLE == 'MCUL18V2_DYJetsToLL' ]; then
+  GT=$GT_MC_2018
+  MINIAOD=${INFILEPREFIX}"/store/mc/RunIISummer20UL18MiniAODv2/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/106X_upgrade2018_realistic_v16_L1v1_ext1-v1/50000/99327950-C31C-9341-BA87-BAC5C7988F14.root"
+  NANOAOD="tree_MCUL18_miniV2_dyjetstoll.root"
   NANOAOD_CFG=JME-RunIISummer20UL18NanoAOD_miniV2_${TAG}_cfg.py
   ERA="Run2_2018,run2_nanoAOD_106Xv2"
   LOGFILE="nanoaod_MCUL18_miniV2.log"
